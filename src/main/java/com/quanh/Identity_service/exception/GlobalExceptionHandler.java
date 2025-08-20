444ee7df661863e60078a0529f64e55b5e8dfaa0
QuangAnh-Ngo <quanganhhp05@gmail.com>
2025-08-20 18:39:03 +0700

    Second commit

--- src/main/java/com/quanh/Identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quanh/Identity_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.quanh.Identity_service.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,5 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String> handlingValidation(MethodArgumentNotValidException exception){
+        return ResponseEntity.badRequest().body(exception.getFieldError().getDefaultMessage());
+        //Default message là mes mình định nghĩa
     }
 }
